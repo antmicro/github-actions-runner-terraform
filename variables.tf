b8f8c51200ee3c2d6731abbc1410f7107400b06c
Adam Olech <aolech@antmicro.com>
2021-07-21 22:26:34 +0200

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "gha_instances_num" {
+  type = number
+}
+
+variable "gha_repository" {
+  type = string
+}
+
+variable "gcp_worker" {
+  type = string
+}
+
+variable "gcp_zone" {
+  type = string
+  default = "us-west1-a"
+}
+
+variable "gcp_project" {
+  type = string
+}
+
+variable "gcp_subnet" {
+  type = string
+}
+
+variable "gcp_image" {
+  type = string
+}
+
+variable "gcp_service_account" {
+  type = string
+}
+
+variable "gcp_disk_type" {
+  type = string
+  default = "pd-ssd"
+}
+
+variable "gcp_disk_size" {
+  type = number
+  default = 30
+}
