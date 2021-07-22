a83c83e3ad96a86abe2f72c67571ec98bbf17294
Adam Olech <aolech@antmicro.com>
2021-07-22 10:42:26 +0200

    Initial resources in main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+locals {
+  zone_no_sub = strrev(substr(strrev(var.gcp_zone), 2, -1))
+}
+
+resource "google_compute_network" "gha-network" {
+  name = var.gcp_subnet
+  project = var.gcp_project
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "gha-subnet" {
+  name = var.gcp_subnet
+  network = google_compute_network.gha-network.id
+  region = local.zone_no_sub
+}
