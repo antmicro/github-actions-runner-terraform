e8d636e2266d3833d76d33d5b2a67b101d6faefa
Adam Olech <aolech@antmicro.com>
2021-07-23 08:19:13 +0200

    Add firewall rules

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 locals {
   zone_no_sub = strrev(substr(strrev(var.gcp_zone), 2, -1))
+  c_tag       = "coordinator"
+  r_tag       = "runners"
 }
 
 resource "google_compute_network" "gha-network" {
@@ -14,4 +16,19 @@ resource "google_compute_subnetwork" "gha-subnet" {
   region        = local.zone_no_sub
   ip_cidr_range = "10.0.0.0/24"
   project       = var.gcp_project
+}
+
+resource "google_compute_firewall" "gha-firewall-allow-unbound" {
+  name        = "${var.gcp_subnet}--coordinator-dns"
+  network     = google_compute_network.gha-network.id
+  direction   = "INGRESS"
+  priority    = 999
+  target_tags = [local.c_tag]
+  source_tags = [local.r_tag]
+  project     = var.gcp_project
+
+  allow {
+    protocol = "udp"
+    ports    = [53]
+  }
 }
