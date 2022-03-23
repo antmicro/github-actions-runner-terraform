bc752b3d687f96b98fca04410e4014fb5d0ee44b
Adam Olech <aolech@antmicro.com>
2022-03-23 11:30:20 +0100

    README: remove unwanted dot

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
  * Copyright (c) 2020-2021 [Antmicro](https://www.antmicro.com)
  *
  * The aim of this project is to simplify the deployment of 
- * [Antmicro's GitHub Actions runner](https://github.com/antmicro/runner).
+ * [Antmicro's GitHub Actions runner](https://github.com/antmicro/runner)
  * and to describe the virtual resources according to IaC principles.
  * 
  * ## Usage
