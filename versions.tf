52aa87afb0a2e01da5e9572307847e5016cf3c96
Adam Olech <aolech@antmicro.com>
2022-11-07 13:24:13 +0100

    versions.tf: bump google provider to 4.42.1

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.53"
+      version = "~> 4.42.1"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 3.53"
+      version = "~> 4.42.1"
     }
   }
 }
